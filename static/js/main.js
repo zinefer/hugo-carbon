d868b33ff6ab19340376cc26c1287a46e0674b36
zinefer <zinefer@gmail.com>
2018-08-15 07:45:09 -0600

    responsive nav

--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,25 @@
 $( document ).ready(function() {
-    $('.image-link').magnificPopup({
-        type: 'image',
-        closeOnContentClick: true,
-        closeBtnInside: false,
-        fixedContentPos: true,
-        mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
-        image: {
-            verticalFit: true
-        },
-        zoom: {
-            enabled: true,
-            duration: 300 // don't foget to change the duration also in CSS
-        }
-    });
+  $('.image-link').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    closeBtnInside: false,
+    fixedContentPos: true,
+    mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+    image: {
+      verticalFit: true
+    },
+    zoom: {
+      enabled: true,
+      duration: 300 // don't foget to change the duration also in CSS
+    }
+  });
 });
+
+function openMenu() {
+  var x = document.getElementById("topbar");
+  if (x.className === "closed") {
+    x.className = "open";
+  } else {
+    x.className = "closed";
+  }
+}
